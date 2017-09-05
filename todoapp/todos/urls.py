550a986433ba00706dfd86b0aa084e2cfc0c18aa
David Lejeune <Lejeune.David@outlook.com>
2017-09-05 14:25:06 +0200

    add new todo + redirect

--- todoapp/todos/urls.py
+++ todoapp/todos/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index , name='index'),
+    url(r'^add', views.add , name='add'),
     url(r'^details/(?P<id>\w{0,50})/$', views.details)
 
 ]
