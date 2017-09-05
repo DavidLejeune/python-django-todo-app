571644763e3cfaff5eb7d190c8f71cdd40b21bda
David Lejeune <Lejeune.David@outlook.com>
2017-09-05 11:35:32 +0200

    pulling out our todos in index.html

--- todoapp/todos/views.py
+++ todoapp/todos/views.py
@@ -4,12 +4,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Todo
+
 # Create your views here.
 
 
 def index(request):
     #return HttpResponse('Hello World')
+    todos = Todo.objects.all()[:10]
     context = {
-        'name': 'David'
+        'todos':todos
     }
     return render(request , 'index.html' , context)
