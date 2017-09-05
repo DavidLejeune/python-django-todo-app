658b32cb5a26004a6babb1598345af1a13bfda81
David Lejeune <Lejeune.David@outlook.com>
2017-09-05 11:10:22 +0200

    addes todos to settings

--- todoapp/todos/models.py
+++ todoapp/todos/models.py
@@ -8,5 +8,4 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_at = models.DateTimeField(default=datetime.now, blank=true)
-    
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
